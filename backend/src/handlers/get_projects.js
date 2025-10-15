cc230db573005aab74457d91fc1237ba79c47fec
Brett Rowland <Lugiakyroke54@gmail.com>
2025-10-14 19:34:39 -0500

    Get Projects Added

---
+++ backend/src/handlers/get_projects.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = async () => {
+    // Make the paramaters for the request
+    const params = {
+        // Table name
+        TableName: process.env.PROJECTS_TABLE,
+        // Grabbing just project_names
+        ProjectionExpression: "project_name"
+    };
+
+    // try catch
+
+    try {
+        const data = await DynamoDB.scan(params).promise();
+        console.log(data)
+        
+        if (data.Count == 0){
+            console.log("No items")
+
+            return {
+                statusCode: 401,
+                body: JSON.stringify({message: 'No Projects Found'})
+            }
+        }
+        return{
+        
+            statusCode: 200,
+            body: JSON.stringify({projects:data.Items})
+        }
+    
+    }catch(error){
+
+        return{
+            statusCode: 500,
+            body: JSON.stringify({message: 'Internal server error',error:error?.message})
+        }
+    }
+
+    // 
+
+
+
+
+
+}
